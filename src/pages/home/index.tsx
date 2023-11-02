d8d07807866c26114b8524ae99488d4b47419333
Matheus <matheus.amaral.1443007@sga.pucminas.br>
2023-11-02 10:31:43 -0300

    terminando o projeto

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,52 +1,124 @@
 import { Social } from "../../components/social"
-import { FaLinkedin, FaInstagram, FaYoutube } from "react-icons/fa"
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa"
+import { useEffect, useState } from "react"
+import { db } from "../../services/firebaseConnection"
+import {
+    getDocs,
+    collection,
+    orderBy,
+    query,
+    doc,
+    getDoc
+
+} from 'firebase/firestore'
+
+interface linkProps {
+    id: string,
+    name: string,
+    url: string,
+    bg: string,
+    color: string,
+}
+
+interface socialProps {
+    facebook: string,
+    instagram: string,
+    youtube: string
+}
+
 
 export function Home() {
+    const [links, setLinks] = useState<linkProps[]>([]);
+    const [socialLinks, setSocialLinks] = useState<socialProps>();
+
+
+    useEffect(() => {
+        function load() {
+        const linksRef = collection(db, "links");
+        const queryRef = query(linksRef, orderBy("created", "asc"));
+
+        getDocs(queryRef)
+        .then((snapshot) => {
+            const lista = [] as linkProps[];
+
+            snapshot.forEach((doc) => {
+                lista.push ({
+                    id: doc.id,
+                    name: doc.data().name,
+                    url: doc.data().url,
+                    bg: doc.data().bg,
+                    color: doc.data().color
+                })
+            })
+            setLinks(lista);
+        })
+        }
+
+        load()
+    }, [])
+
+    useEffect(() => {
+       function loadsocial() {
+        const docRef = doc(db, "social", "links");
+        getDoc(docRef)
+        .then((snapshot) => {
+                if(snapshot.data() !== undefined) {
+                    setSocialLinks({
+                        facebook: snapshot.data()?.facebook,
+                        instagram: snapshot.data()?.instagram,
+                        youtube: snapshot.data()?.youtube,
+                    })
+                }
+        }) 
+       }
+
+       loadsocial();
+    }, [])
+
     return (
         <div className="flex flex-col w-full py-4 items-center justify-center">
             <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Matheus Amaral</h1>
             <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
             <main className="flex flex-col w-11/12 max-w-xl text-center">
-                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-tranform hover:scale-105 transition-all cursor-pointer">
-                    <a>
-                        <p className="text-base text-black md:text-lg">
-                            Acesse meu Instagram
-                        </p>
-                    </a>
-                </section>
+                {
+                    links.map((item) => (
+                        <section 
+                            className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-tranform hover:scale-105 transition-all cursor-pointer"
+                            key={item.id}
+                            style={{backgroundColor: item.bg}}
+                            id="link"
+                        >
+                            <a href={item.url} target="_blank">
+                                <p 
+                                    className="text-base  md:text-lg"
+                                    style={{color: item.color}}
+                                >
+                                   {item.name}
+                                </p>
+                            </a>
+                        </section>
+                    ))
+                }
 
-                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-tranform hover:scale-105 transition-all cursor-pointer">
-                    <a>
-                        <p className="text-base text-black md:text-lg">
-                            Acesso meu youtube
-                        </p>
-                    </a>
-                </section>
+            {
+                socialLinks && (
+                    <footer className="flex justify-center gap-3 my-4">
+                        <Social url={socialLinks?.facebook}>
+                            <FaFacebook size={35} color="#fff"/>
+                        </Social>
 
-                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-tranform hover:scale-105 transition-all cursor-pointer">
-                    <a>
-                        <p className="text-base text-black md:text-lg">
-                            Acesse meu Linkedin
-                        </p>
-                    </a>
-                </section>
-
-                <footer className="flex justify-center gap-3 my-4">
-
-                    <Social url="https://www.linkedin.com/in/matheus-amaral-00762b265/">
-                        <FaLinkedin size={35} color="#fff"/>
-                    </Social>
-
-                    <Social url="https://www.youtube.com/channel/UCek7dMY4R5qotAa-wXcB_XA">
-                        <FaYoutube size={35} color="#fff"/>
-                    </Social>
-                    
-                    <Social url="https://www.instagram.com/uomatheus/">
-                        <FaInstagram size={35} color="#fff"/>
-                    </Social>
-                    
-                </footer>
+                        <Social url={socialLinks?.youtube}>
+                            <FaYoutube size={35} color="#fff"/>
+                        </Social>
+                        
+                        <Social url={socialLinks?.instagram}>
+                            <FaInstagram size={35} color="#fff"/>
+                        </Social>
+                        
+                    </footer>
+                )
+            }
             </main>
         </div>
     )
--- src/pages/networks/index.tsx
+++ src/pages/networks/index.tsx
@@ -1,11 +1,51 @@
 import { Header } from "../../components/header"
-import { Input } from "../../components/Input"
-import { useState } from 'react'
+
+import { FormEvent, useEffect, useState } from 'react'
+
+import { db } from '../../services/firebaseConnection';
+import {
+    setDoc,
+    doc,
+    getDoc
+} from 'firebase/firestore';
 
 export function Networks() {
 
     const [facebook, setFacebook] = useState("");
     const [instagram, setInstagram] = useState("");
+    const [youtube, setYoutube] = useState("");
+
+    useEffect(() => {
+        function loadlinks() {
+            const docRef = doc(db, "social", "link");
+            getDoc(docRef)
+            .then((snapshot) => {
+               if (snapshot.data() !== undefined) {
+                    setFacebook(snapshot.data()?.facebook);
+                    setInstagram(snapshot.data()?.instagram);
+                    setYoutube(snapshot.data()?.youtube);
+               }
+            })
+        }  
+        
+        loadlinks();
+    }, [])
+
+    function handleRegister(e: FormEvent) {
+        e.preventDefault();
+        
+        setDoc(doc(db, "social", "link"), {
+            facebook: facebook,
+            instagram: instagram,
+            youtube: youtube
+        })
+        .then(() => {
+            console.log("Cadastrado com sucesso");
+        })
+        .catch((err) => {
+            console.log("Erro ao cadastrar", err);
+        })
+    }
 
     return(
         <div className="flex flex-col min-h-screen max-w-screen-4xl gap-3">
@@ -13,7 +53,7 @@ export function Networks() {
             
             <h1 className=" text-white text-2xl font-medium mt-8 mb-4 text-center">Minhas redes sociais</h1>
 
-                <form className=" flex flex-col gap-2 p-1 items-center">
+                <form className=" flex flex-col gap-2 p-1 items-center" onSubmit={handleRegister}>
                     <label className="text-white font-medium mt-2 mb-2">Link do facebook</label>
                     <input 
                         type="url"
@@ -31,6 +71,24 @@ export function Networks() {
                         value={instagram}
                         onChange={(e) => setInstagram(e.target.value)}
                     />
+
+                    <label className="text-white font-medium mt-2 mb-2">Link do youtube</label>
+                    <div className="w-full max-w-2xl flex flex-col gap-10">
+                    <input 
+                        type="url"
+                        placeholder="Digite a url do youtube"
+                        className=" w-full max-w-2xl rounded p-2 text-black"
+                        value={youtube}
+                        onChange={(e) => setYoutube(e.target.value)}
+                    />
+
+                    <button 
+                        type="submit"
+                        className="text-white w-full max-w-2xl p-5 bg-blue-600 h-9 rounded-md items-center justify-center flex mb-7 font-medium"    
+                    >
+                        Salvar Links
+                    </button>
+                    </div>
                 </form>
         </div>
     )
